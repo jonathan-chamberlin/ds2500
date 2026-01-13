0a7f6129912d4c6f523cdad0ce9c3fa59d89ac1e
Jonathan Chamberlin <jcham17x@gmail.com>
2026-01-13 11:27:17 -0500

    changed

--- main.py
+++ main.py
@@ -16,3 +16,22 @@ print(text2[0:6])
 print(text2.replace("Python","Java"))
 
 print(text2)
+
+s = 'hello world. this sentance will print in the next line. this is another sentance.'
+print(s)
+
+print(len(s.split(".")))
+print(len(s.split(" ")))
+words = s.split(" ")
+print(words)
+
+sentences = s.split(".")
+sentences = sentences[:-1]
+print(f"sentances: {sentences}")
+print(60*"=")
+
+numbers = [0,1,2,3,4,5]
+
+print(numbers[2])
+numbers[5] = 10
+print(numbers)
