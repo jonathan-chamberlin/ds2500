00388577398e7dd61d4597110a6f96b73f43c6c7
Jonathan Chamberlin <jcham17x@gmail.com>
2026-01-13 10:43:30 -0500

    string exercises

---
+++ main.py
@@ -0,0 +1,18 @@
+text = "Hello World"
+
+print(text[::-1])
+print("end")
+
+print("wow".join([text,"!!"]))
+
+text2 = "Python Programming"
+
+print(text2[0])
+print(text2[-1])
+print(len(text2))
+print(text2.lower())
+
+print(text2[0:6])
+print(text2.replace("Python","Java"))
+
+print(text2)
