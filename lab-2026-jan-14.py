5c83458ac72098508c9b399e963126f56a9e7f8c
Jonathan Chamberlin <jcham17x@gmail.com>
2026-01-14 14:05:22 -0500

    edits

--- lab-2026-jan-14.py
+++ lab-2026-jan-14.py
@@ -9,10 +9,10 @@ is_active = True
 text = "Data Science"
 first_char = text[0]
 last_char = text[-1]
-len(text)
+text_length = len(text)
 text_upper = text.upper()
 second_word = text.split(" ")[1]
-text.replace("Data","Computer")
+text1 = text.replace("Data","Computer")
 
 # Problem 3
 fruits = ["banana", "apple", "orange"]
@@ -24,7 +24,6 @@ first_item = fruits[0]
 last_item = fruits[-1]
 
 # Problem 4
-spam = "Spam spam Spam spam Spam spam Spam Spam Spam spam spams spams spams spams spams spams spams Spam Spam Spam Spams Spam spam spam Spam spams spams Spam Spam Spam Spams Spam spam spam Spam spams spam spam spams spams"
 
 spam = """Spam spam Spam spam Spam spam Spam Spam Spam spam spams spams
 spams spams spams spams spams Spam Spam Spam Spams Spam spam spam Spam
@@ -40,7 +39,7 @@ count_spams = spam.count("spams")
 spam_clean = spam.lower().replace("spams", "spam")
 
 # count spam in the cleaned string
-count_spam_clean = spam_clean.count("spam")
+count_spam_clean = spam_clean.split().count("spam")
 
 # convert to list of words
 spam_list = spam_clean.split()
